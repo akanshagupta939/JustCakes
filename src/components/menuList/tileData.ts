129e6b5420b4d4201f7d9e40c3dc2628e4983aec
akansha010293 <86750977+akansha010293@users.noreply.github.com>
2021-07-28 23:23:55 +1000

    added width and height

--- src/components/menuList/tileData.ts
+++ src/components/menuList/tileData.ts
@@ -71,6 +71,8 @@ export const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     position: "relative",
+    width: "100%",
+    height: "100%",
     top: 0,
     bottom: 0,
     fontFamily: "OldStandardTT-Regular, Arial, sans-serif",
